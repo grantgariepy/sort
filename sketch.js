a336e341f3ea3afc1471963c413a4dbd202e404d
grantgariepy <grantgariepy@protonmail.com>
2022-03-14 19:32:36 -0600

    SORTING

--- sketch.js
+++ sketch.js
@@ -1,39 +1,81 @@
-let i = 0;
-let w = 10;
+let values = [];
+let w = 5;
+
+let states = [];
 
 function setup() {
-  createCanvas(800, 200);
+  createCanvas(windowWidth, windowHeight);
   values = new Array(floor(width / w));
-  for (let i = 0, i < values.length; i++) {
+  for (let i = 0; i < values.length; i++) {
     values[i] = random(height);
+    states[i] = -1;
   }
-  frameRate(5);
-  quickSort(values, 0, values.length-1);
+  quickSort(values, 0, values.length - 1);
 }
 
-
-function quickSort(arr, start, end) {
+async function quickSort(arr, start, end) {
   if (start >= end) {
     return;
   }
+  let index = await partition(arr, start, end);
+  states[index] = -1;
 
-  let index = partition(arr, start, end);
-  quickSort(arr, start, index-1);
-  quickSort(arr, index + 1, end);
+  await Promise.all([
+    quickSort(arr, start, index - 1),
+    quickSort(arr, index + 1, end)
+  ]);
+}
+
+async function partition(arr, start, end) {
+  for (let i = start; i < end; i++) {
+    states[i] = 1;
+  }
+
+  let pivotValue = arr[end];
+  let pivotIndex = start;
+  states[pivotIndex] = 0;
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivotValue) {
+      await swap(arr, i, pivotIndex);
+      states[pivotIndex] = -1;
+      pivotIndex++;
+      states[pivotIndex] = 0;
+    }
+  }
+  await swap(arr, pivotIndex, end);
+
+  for (let i = start; i < end; i++) {
+    if (i != pivotIndex) {
+      states[i] = -1;
+    }
+  }
+
+  return pivotIndex;
 }
 
 function draw() {
-  background(51);
-}
+  background(0);
 
   for (let i = 0; i < values.length; i++) {
-    stroke(0);
-    fill(255);
+    noStroke();
+    if (states[i] == 0) {
+      fill('#1e62ff');
+    } else if (states[i] == 1) {
+      fill('#ab11f3');
+    } else {
+      fill(233);
+    }
     rect(i * w, height - values[i], w, values[i]);
   }
+}
 
-function swap(arr, a, b) {
+async function swap(arr, a, b) {
+  await sleep(1);
   let temp = arr[a];
   arr[a] = arr[b];
   arr[b] = temp;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
